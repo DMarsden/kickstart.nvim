a51f69ebf6da7a21a5138da36331043d1668da24
Dan Marsden <danmarsden@yahoo.co.uk>
2025-12-15 19:11:27 +0000

    sorting out the tabs versus spaces mess

--- init.lua
+++ init.lua
@@ -205,6 +205,11 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Set global indentation settings for all file types
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
