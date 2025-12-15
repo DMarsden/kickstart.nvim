ae0d246478415d2051468a18c0af4dbe793c9e19
Dan Marsden <danmarsden@yahoo.co.uk>
2025-12-15 21:49:50 +0000

    rust gubbins

--- init.lua
+++ init.lua
@@ -253,7 +253,11 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^6', -- Recommended
+    lazy = false,
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
