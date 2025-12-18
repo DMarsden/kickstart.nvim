ec0ae6d1831f094a67934d2c346f9ccef3e5e8cc
Dan Marsden <danmarsden@yahoo.co.uk>
2025-12-18 11:38:22 +0000

    slightly more realistic snips using more features

--- lua/snippets/init.lua
+++ lua/snippets/init.lua
@@ -1,8 +1,12 @@
 local luasnip = require 'luasnip'
+local fmt = require('luasnip.extras.fmt').fmt
 local s = luasnip.snippet
 local t = luasnip.text_node
+local i = luasnip.insert_node
 luasnip.add_snippets('all', {
-  s('hi', {
-    t 'hello, world',
-  }),
+  s('yl', { t 'Yoneda lemma' }),
+  s('ye', { t 'Yoneda embedding' }),
+})
+luasnip.add_snippets('all', {
+  s('%is', fmt('%include TikZ/{}.snip', { i(1) })),
 })
