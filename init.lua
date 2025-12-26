c83fb8e78fffee1fc4a50efe228c3abde58bab8e
Dan Marsden <danmarsden@yahoo.co.uk>
2025-12-26 10:20:00 +0000

    Adding extra treesitter languages
    toml and json installed by default

--- init.lua
+++ init.lua
@@ -1051,7 +1051,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'rust' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'rust', 'toml', 'json' },
       -- Autoinstall languages that are not installed
       auto_install = false,
       highlight = {
